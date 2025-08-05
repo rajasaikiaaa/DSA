ae3bfe751b18304b630b1a361673a79e1b861d99
Raja Saikia <rajasaikiaaa@gmail.com>
2025-08-05 09:56:37 +0530

    brute force

---
+++ pow.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+/*
+//brute  force approach;
+
+    double x = 10;
+    int n = -2;
+    //assigning a variale to store the power
+    long double pow = 1;
+
+    //checking if 'n' is positive or negative
+    if( n<0){
+        //n is negative and hence it is convereted to positive using the abs built-in function
+        int con = abs(n);
+        //iterating the 'x' for "n" number of times;
+        for( int i=1; i<=con; i++){
+            //storing and multiplying 'x'
+            pow = pow * x;
+        }
+        //lastly after computing the squares of 'x' , it is divided by 1 to get the final ans;
+        double ans = 1/pow;
+        cout << ans << endl;
+        //if 'n' is positive;
+    } else {
+        //running loop just to multiply 'x' for 'n' times;
+        for( int i=1; i<= n; i++){
+            pow = pow * x;
+        }
+        cout << pow << endl;
+    }*/
+    
+
+    //time complexity is O(n); linear;
+
+
+
+    //optimal approach;
+
+
+
+    return 0;
+}
